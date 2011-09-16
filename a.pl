7d1f88fdb2940257973b594759729bdf52169b5b
Eric Brine <ikegami@adaelis.com>
2011-09-16 01:13:20 -0400

    Prototype

---
+++ a.pl
@@ -0,0 +1,152 @@
+
+# perl -MInline=FORCE,INFO,NOISY,NOCLEAN a.pl
+
+use strict;
+use warnings;
+use feature qw( say );
+
+use Inline C => <<'__EOI__';
+
+
+STATIC int (*next_keyword_plugin)(pTHX_ char*, STRLEN, OP**);
+
+
+STATIC void croak_missing_terminator(pTHX_ I32 edelim) {
+   dVAR;
+   char* s;
+   char q;
+
+   if (edelim == -1)
+      Perl_croak(aTHX_ "qw not terminated anywhere before EOF");
+
+   if (isCNTRL(edelim)) {
+      s = ""; // ~~~ ^ plus toCTRL(PL_multi_close)
+   }
+   else {
+      s = ""; // ~~~
+   }
+
+   q = strchr(s, '"') ? '\'' : '"';
+   Perl_croak(aTHX_ "Can't find qw terminator %c%s%c anywhere before EOF", q, s, q);
+}
+
+
+// sv is assumed to contain a string (and nothing else).
+// sv is assumed to have no magic.
+// The sv's length is reduced to zero length and the UTF8 flag is turned off.
+STATIC void append_word_to_list(OP** list_op_ptr, SV* word_sv) {
+   STRLEN len = SvCUR(word_sv);
+   if (len) {
+      SV* sv_copy = newSV(len);
+      sv_copypv(sv_copy, word_sv);
+// ~~~ Check size of sv_copy's buffer.
+      *list_op_ptr = op_append_elem(OP_LIST, *list_op_ptr, newSVOP(OP_CONST, 0, sv_copy));
+      
+      SvCUR_set(word_sv, 0);
+      SvUTF8_off(word_sv);
+   }
+}
+
+STATIC void sv_catuvchr_nomg(SV* sv, UV uv) {
+   if (SvUTF8(sv) || uv > 255) {
+      char buf[UTF8_MAXBYTES+1];  // I wonder why the "+ 1".
+      STRLEN len;
+      len = (char*)uvuni_to_utf8((U8*)buf, uv) - buf;
+      sv_utf8_upgrade_flags_grow(sv, 0, len+1);
+      sv_catpvn_nomg(sv, buf, len);
+   } else {
+      sv_catpvn_nomg(sv, &((char)uv), 1);
+   }
+}
+
+
+// XXX ~~~ croak_missing_terminator causes list_op and word to leak.
+STATIC OP * parse_qw(pTHX) {
+   I32 sdelim;
+   I32 edelim;
+   IV depth;
+   OP * list_op = newLISTOP(OP_LIST, 0, NULL, NULL);
+   SV * word_sv = newSVpvn("", 0);
+
+   lex_read_space(0);
+
+   sdelim = lex_read_unichar(0);
+   if (sdelim == -1)
+      croak_missing_terminator(aTHX_ -1);
+
+   { // Find corresponding closing delimiter
+      char* p;
+      if (sdelim && (p = strchr("([{< )]}> )]}>", sdelim)))
+         edelim = *(p + 5);
+      else
+         edelim = sdelim;
+   }
+
+   depth = 1;
+   for (;;) {
+      I32 c = lex_peek_unichar(0);
+      if (c == -1)
+         croak_missing_terminator(aTHX_ edelim);
+      if (c == edelim) {
+         append_word_to_list(&list_op, word_sv);
+         lex_read_unichar(0);
+         if (!--depth)
+            break;
+         sv_catuvchr_nomg(word_sv, c);
+      }
+      else if (c == sdelim) {
+         lex_read_unichar(0);
+         ++depth;
+         sv_catuvchr_nomg(word_sv, c);
+      }
+      else if (c == '\\') {
+         lex_read_unichar(0);
+         c = lex_peek_unichar(0);
+         if (c == -1)
+             croak_missing_terminator(aTHX_ edelim);
+         if (c != sdelim && c != edelim && c != '\\')
+            sv_catuvchr_nomg(word_sv, '\\');
+         lex_read_unichar(0);
+         sv_catuvchr_nomg(word_sv, c);
+      }
+      else if (c == '#' || isSPACE(c)) {
+         append_word_to_list(&list_op, word_sv);
+         lex_read_space(0);
+      }
+      else {
+         lex_read_unichar(0);
+         sv_catuvchr_nomg(word_sv, c);
+      }
+   }
+
+   SvREFCNT_dec(word_sv);
+
+   return list_op;
+}
+
+
+STATIC int my_keyword_plugin(pTHX_ char* keyword_ptr, STRLEN keyword_len, OP** op_ptr) {
+   if (keyword_len == 2 && keyword_ptr[0] == 'q' && keyword_ptr[1] == 'w') {
+      *op_ptr = parse_qw(aTHX);
+      return KEYWORD_PLUGIN_EXPR;
+   }
+
+   return next_keyword_plugin(aTHX_ keyword_ptr, keyword_len, op_ptr);
+}
+
+
+void init() {
+   next_keyword_plugin = PL_keyword_plugin;
+   PL_keyword_plugin = my_keyword_plugin;
+}
+
+__EOI__
+
+
+BEGIN { init(); }
+
+say qw! foo bar !, 'baz';
+
+# ~~~ To test: say for qw! foo bar ! x 3;
+# ~~~ Set UTF8 flag?
+# ~~~ Warn for commas
